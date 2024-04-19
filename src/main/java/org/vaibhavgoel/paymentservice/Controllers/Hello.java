a3a955dceae5dbcc53d6284dfa01b0bcc0872d09
Vaibhav Goel <p0083@cdot.in>
2024-04-19 23:33:24 +0530

    InitialCommit

---
+++ src/main/java/org/vaibhavgoel/paymentservice/Controllers/Hello.java
@@ -0,0 +1,12 @@
+package org.vaibhavgoel.paymentservice.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
+}
